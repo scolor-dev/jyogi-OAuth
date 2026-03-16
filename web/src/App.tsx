3fb2ec4959aeb186493b19072686b5c273b31ae4
izumi-rui23 <r-izumi@e3sys.co.jp>
2026-03-16 15:10:43 +0900

    fix:BrowserRouterの位置修正

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import RootLayout from './components/layouts/RootLayout'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -6,14 +6,12 @@ import Info from './pages/Info/Info'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RootLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="info" element={<Info />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="info" element={<Info />} />
+      </Route>
+    </Routes>
   )
 }
